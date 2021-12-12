ff32e84faa242e7d55e11e75ecba80f071c2d828
PallaGergelyIstvan <93809122+PallaGergelyIstvan@users.noreply.github.com>
2021-12-12 16:29:37 +0100

    Start
    Start

---
+++ src/main/java/org/example/VizsgaRFT/VizsgaRftApplication.java
@@ -0,0 +1,13 @@
+package org.example.VizsgaRFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VizsgaRftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VizsgaRftApplication.class, args);
+	}
+
+}
